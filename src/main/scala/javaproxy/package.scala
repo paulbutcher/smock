79643cee7d6b3f775fb2a5e6523419f3c0b1e4b0
Paul Butcher <paul@paulbutcher.com>
2011-02-06 03:03:55 +0000

    Remove unused files

--- src/main/scala/javaproxy/package.scala
+++
@@ -1,7 +0,0 @@
-package com.paulbutcher.smock
-
-package object javaproxy {
-  def allInterfaces(cl: Class[_]): List[Class[_]] =
-    if (cl == null) Nil
-    else cl.getInterfaces.toList ++ allInterfaces(cl.getSuperclass) distinct
-}
--- src/test/scala/MockSpec.scala
+++ src/test/scala/MockSpec.scala
@@ -6,10 +6,19 @@ class MockSpec extends WordSpec with MockFactory {
   
   "A mock" should {
     
+    "Succeed if no expectations are setup and no methods are called" in {
+      val t = mock[Turtle]
+    }
+    
+    "Fail if an unexpected method is called" in {
+      val t = mock[Turtle]
+      intercept[ExpectationException] { t.penDown }
+    }
+    
     "Succeed if an expected method is called" in {
       
       val t = mock[Turtle]
-      t.expects('penDown).once
+      t.expects('penDown)
       t.penDown
     }
   }
